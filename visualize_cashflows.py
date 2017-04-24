ba2a3dda998f0ecb055d05f211190f3810517a32
Adam Behrman <ab4017@MacBook-Pro-3.fios-router.home>
2017-04-23 21:04:37 -0400

    formatting looks better, shared selection, data table works properly

--- visualize_cashflows.py
+++ visualize_cashflows.py
@@ -1,9 +1,9 @@
 import numpy as np
-from bokeh.layouts import widgetbox, gridplot
+from bokeh.layouts import widgetbox, layout
 # from bokeh.io import output_file, show
 from bokeh.models import NumeralTickFormatter, ColumnDataSource
-from bokeh.models.widgets import Button
-from bokeh.models.widgets import Slider
+from bokeh.models.tools import HoverTool
+from bokeh.models.widgets import Button, Slider, DataTable, NumberFormatter, TableColumn
 from bokeh.plotting import figure, curdoc
 
 import collateral_waterfall as cw
@@ -26,58 +26,106 @@ controls = [psa_slider, wam_slider, original_balance_slider, calc_button]
 #    control.on_change('value', lambda attr, old, new: update())
 
 wam = 358
-periods = range(1, wam + 1)
+window = range(1, wam + 1)
 
 # PSA graph
 
-psa_figure = figure(title='PSA speed')
-for mult in np.linspace(0.25, 3, num=12):
-    x = []
-    y = []
+psa_figure = figure(title='PSA speed', tools=['box_zoom', 'lasso_select', 'box_select', 'save', 'reset'])
 
-    for period in periods:
-        x.append(period)
-        y.append(pc.PSA(period) * mult)
-    psa_figure.line(x, y, name='PSA-{0:.2f}'.format(mult), alpha=0.3)
+for mult in np.linspace(0.25, 3, num=12):
+    periods = []
+    psa_speed = []
+
+    for period in window:
+        periods.append(period)
+        psa_speed.append(pc.PSA(period) * mult)
+    psa_figure.line(periods, psa_speed, name='PSA-{0:.2f}'.format(mult), alpha=0.3)
 
 psa_figure.yaxis.formatter = NumeralTickFormatter(format='0%')
 
 # Waterfall graphs
 
-waterfall_figure = figure(title='Waterfall')
+waterfall_figure = figure(title='Waterfall',
+                          tools=['box_zoom', 'lasso_select', 'box_select', 'save', 'reset', HoverTool(tooltips=[
+                              ('Month:', '@periods{int}'),
+                              ('SMM:', '@SMM{0.00000}'),
+                              ('Mortgage Payments:', '@mortgage_payments{"$,"}'),
+                              ('Net Interest:', '@net_interest{"$,"}'),
+                              ('Scheduled Principal:', '@scheduled_principal{"$,"}'),
+                              ('Prepayments:', '@prepayments{"$,"}'),
+                              ('Total Principal:', '@total_principal{"$,"}'),
+                              ('Total Cash Flow:', '@cash_flow{"$,"}'),
+                          ])])
 
 psa_speeds = [1.0, 1.65]
-source = ColumnDataSource(data=dict(periods=[], psa_speed=[], cash_flow=[]))
+source = ColumnDataSource(data=dict(periods=[],
+                                    psa_speed=[],
+                                    cash_flow=[],
+                                    beginning_balance=[],
+                                    SMM=[],
+                                    mortgage_payments=[],
+                                    net_interest=[],
+                                    prepayments=[],
+                                    scheduled_principal=[],
+                                    total_principal=[]))
 
+columns = [
+    TableColumn(field='periods', title='Month', formatter=NumberFormatter(format=',')),
+    TableColumn(field='beginning_balance', title='Beginning Balance', formatter=NumberFormatter(format=',')),
+    TableColumn(field='SMM', title='SMM', formatter=NumberFormatter(format='0.00000')),
+    TableColumn(field='mortgage_payments', title='Mortgage Payments', formatter=NumberFormatter(format=',')),
+    TableColumn(field='net_interest', title='Net Interest', formatter=NumberFormatter(format=',')),
+    TableColumn(field='scheduled_principal', title='Scheduled Principal', formatter=NumberFormatter(format=',')),
+    TableColumn(field='prepayments', title='Prepayments', formatter=NumberFormatter(format=',')),
+    TableColumn(field='total_principal', title='Total Principal', formatter=NumberFormatter(format=',')),
+    TableColumn(field='cash_flow', title='Total Cash Flow', formatter=NumberFormatter(format=',')),
+]
 
-def update():
-    source.data['periods'] = []
-    source.data['psa_speed'] = []
-    source.data['cash_flow'] = []
-
-    speed = psa_slider.value
-    wam = wam_slider.value
-    bal = original_balance_slider.value
-
-    periods = range(1, wam + 1)
-
-    for period in periods:
-        source.data['periods'].append(period)
-        source.data['psa_speed'].append(pc.PSA(period) * speed)
-    psa_figure.line(x='periods', y='psa_speed', source=source, name='PSA-{0:.2f}'.format(mult), alpha=1, color='red')
-
-    waterfall = cw.create_waterfall(original_balance=bal, psa_speed=speed, wam=wam)
-    source.data['cash_flow'] = waterfall.cash_flow.tolist()
-
-    waterfall_figure.line(x='periods', y='cash_flow', color='blue', source=source)
-
-    print(len(source.data['periods']), len(source.data['psa_speed']), len(source.data['cash_flow']))
+data_table = DataTable(source=source, columns=columns, fit_columns=True, width=1200, row_headers=False)
 
 waterfall_figure.x_range = psa_figure.x_range
 waterfall_figure.xaxis.axis_label = 'Month of Payment'
 waterfall_figure.yaxis.axis_label = 'Total Cash Flows ($)'
 waterfall_figure.xaxis.formatter = NumeralTickFormatter(format='0')
 waterfall_figure.yaxis.formatter = NumeralTickFormatter(format=',')
+
+def update():
+
+    speed = psa_slider.value
+    wam = wam_slider.value
+    bal = original_balance_slider.value
+
+    source.data['periods'] = []
+    source.data['psa_speed'] = []
+    source.data['cash_flow'] = list(np.zeros(wam))
+    source.data['beginning_balance'] = list(np.zeros(wam))
+    source.data['SMM'] = list(np.zeros(wam))
+    source.data['mortgage_payments'] = list(np.zeros(wam))
+    source.data['net_interest'] = list(np.zeros(wam))
+    source.data['prepayments'] = list(np.zeros(wam))
+    source.data['scheduled_principal'] = list(np.zeros(wam))
+    source.data['total_principal'] = list(np.zeros(wam))
+
+    periods = range(1, wam + 1)
+
+    for period in window:
+        source.data['periods'].append(period)
+        source.data['psa_speed'].append(pc.PSA(period) * speed)
+    psa_figure.circle(x='periods', y='psa_speed', source=source, name='PSA-{0:.2f}'.format(mult), alpha=1, color='red')
+
+    waterfall = cw.create_waterfall(original_balance=bal, psa_speed=speed, wam=wam)
+    source.data['cash_flow'] = waterfall.cash_flow.tolist()
+    source.data['beginning_balance'] = waterfall.beginning_balance.tolist()
+    source.data['SMM'] = waterfall.SMM.tolist()
+    source.data['mortgage_payments'] = waterfall.mortgage_payments.tolist()
+    source.data['net_interest'] = waterfall.net_interest.tolist()
+    source.data['prepayments'] = waterfall.prepayments.tolist()
+    source.data['scheduled_principal'] = waterfall.scheduled_principal.tolist()
+    source.data['total_principal'] = waterfall.total_principal.tolist()
+
+    waterfall_figure.circle(x='periods', y='cash_flow', color='blue', source=source)
+
+    print(len(source.data['beginning_balance']), len(source.data['psa_speed']), len(source.data['cash_flow']))
 
 # output_file('waterfalls.html')
 #show(grid)
@@ -86,7 +134,10 @@ calc_button.on_click(update)
 
 inputs = widgetbox(*controls)
 
-grid = gridplot([inputs], [psa_figure, waterfall_figure])
+grid = layout([
+    [inputs],
+    [data_table],
+    [psa_figure, waterfall_figure]])  # , sizing_mode='stretch_both')
 
 update()
 
