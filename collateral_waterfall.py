34445c526f4e22dc5f032d00b0e194abfd3cf2de
Adam Behrman <ab4017@MacBook-Pro-3.fios-router.home>
2017-05-02 20:53:10 -0400

    actual balances and arm_coupons added, balance schedules; work to collaeral_waterfall

--- collateral_waterfall.py
+++ collateral_waterfall.py
@@ -43,13 +43,13 @@ def create_waterfall(original_balance=400e6, pass_thru_cpn=0.055, wac=0.06, wam=
             row['beginning_balance'] = waterfall['beginning_balance'].ix[ix - 1] - waterfall['total_principal'].ix[
                 ix - 1]
 
-        row['mortgage_payments'] = -np.pmt(rate=wac / 12,
+        row['mortgage_payments'] = -np.pmt(rate=wac / 12.,
                                            nper=wam - ix + 1,
                                            pv=row['beginning_balance'],
-                                           fv=0,
+                                           fv=0.,
                                            when='end')
 
-        row['net_interest'] = row['beginning_balance'] * pass_thru_cpn / 12
+        row['net_interest'] = row['beginning_balance'] * pass_thru_cpn / 12.
         gross_coupon = row['beginning_balance'] * (wac / 12.)
 
         row['scheduled_principal'] = row['mortgage_payments'] - gross_coupon
@@ -63,64 +63,7 @@ def create_waterfall(original_balance=400e6, pass_thru_cpn=0.055, wac=0.06, wam=
     return waterfall
 
 
-def create_waterfall_enhanced(scheduled_balances=400e6, pass_thru_cpn=0.055, wac=0.06, wam=358, psa_speed=1.0,
-                              cpr_description='.2 ramp 6 for 30, 6'):
-    """ Takes collateral summary inputs based on aggregations equaling total original balance, average pass-thru-coupon,
-    weighted average coupon of underlying loans, weighted average maturity of underlying loans, psa speed multiplier
-    for prepayment curve, and constant prepayment rate curve description.
-
-    CPR description is turned into a list of CPRs which are then run through the SMM function for period SMMs."""
-
-    cpr_curve = pc.cpr_curve_creator(cpr_description)
-    age = 360 - wam
-
-    index = pd.Index(range(1, wam + 1), name='month')
-
-    beg_balance = np.zeros(wam)
-    smm = np.array([pc.SMM(cpr_curve[period + age - 1] * psa_speed) for period in index])
-
-    rem_cols = pd.DataFrame({
-        'mortgage_payments': np.zeros(wam),
-        'net_interest': np.zeros(wam),
-        'scheduled_principal': np.zeros(wam),
-        'prepayments': np.zeros(wam),
-        'total_principal': np.zeros(wam),
-        'cash_flow': np.zeros(wam)
-    }, index=index)
-
-    waterfall = pd.DataFrame(data=[beg_balance, smm]).T
-    waterfall.columns = ['beginning_balance', 'SMM']
-    waterfall.index = index
-    waterfall = waterfall.join(rem_cols)
-
-    for ix, row in waterfall.iterrows():
-        if ix == 1:
-            row['beginning_balance'] = original_balance
-        else:
-            row['beginning_balance'] = waterfall['beginning_balance'].ix[ix - 1] - waterfall['total_principal'].ix[
-                ix - 1]
-
-        row['mortgage_payments'] = -np.pmt(rate=wac / 12,
-                                           nper=wam - ix + 1,
-                                           pv=row['beginning_balance'],
-                                           fv=0,
-                                           when='end')
-
-        row['net_interest'] = row['beginning_balance'] * pass_thru_cpn / 12
-        gross_coupon = row['beginning_balance'] * (wac / 12.)
-
-        row['scheduled_principal'] = row['mortgage_payments'] - gross_coupon
-
-        row['prepayments'] = row['SMM'] * (row['beginning_balance'] - row['scheduled_principal'])
-
-        row['total_principal'] = row['scheduled_principal'] + row['prepayments']
-        row['total_principal'] = row['scheduled_principal'] + row['prepayments']
-        row['cash_flow'] = row['net_interest'] + row['total_principal']
-
-    return waterfall
-
-
-def scheduled_balances(rate, nper, pv):
+def schedule_of_ending_balances(rate, nper, pv):
     """ Returns data frame of scheduled balances and each periods scheduled balance as a %
     of the original balance"""
 
@@ -128,21 +71,45 @@ def scheduled_balances(rate, nper, pv):
                       index=[np.arange(0, nper + 1)])
     df.loc[0, 'scheduled_balance'] = pv
 
-    df['bal_percent'] = (1 - ((((1 + rate) ** np.array(df.index)) - 1) /
-                              (((1 + rate) ** nper) - 1)))
+    df['bal_percent'] = (1. - (((1. + rate / 12.) ** np.array(df.index) - 1.) /
+                               ((1. + rate / 12.) ** nper - 1.)))
 
     df.loc[:, 'scheduled_balance'] = df['bal_percent'] * df.loc[0, 'scheduled_balance']
 
     return df
 
 
-def scheduled_balance_percent_for_period(rate, nper, age):
-    return (1 - ((((1 + rate) ** age) - 1) /
-                 (((1 + rate) ** nper) - 1)))
+def schedule_of_ending_balance_percent_for_period(rate, nper, age):
+    return (1. - ((((1 + rate) ** age) - 1.) /
+                  (((1 + rate) ** nper) - 1.)))
 
 
-def actual_balances(scheduled_balances, smm):
+def actual_balances(ending_balances, smm):
     """ Returns vector of actual balances and their fractional composition of scheuled"""
+    # age = 361 - len(ending_balances)
+
+    return ending_balances[:-1] * (1 - np.array(smm))
+
+
+def arm_coupons(rate_curve,
+                gross_margin,
+                total_fees,
+                initial_coupon,
+                periodic_cap):
+    df = pd.DataFrame(index=pd.Index(range(len(rate_curve))),
+                      columns=['rates', 'Gross', 'Net'])
+    df.rates = rate_curve
+    df.loc[0, 'Gross'] = initial_coupon
+
+    for i in range(1, df.index.max() + 1):
+        if df.rates[i] + gross_margin <= df.Gross[i - 1] + periodic_cap:
+            df.loc[i, 'Gross'] = df.rates[i] + gross_margin
+        else:
+            df.loc[i, 'Gross'] = df.Gross[i - 1] + periodic_cap
+
+    df.loc[:, 'Net'] = df.Gross - total_fees
+
+    return df
 
 
 def example():
@@ -153,13 +120,15 @@ def example():
     for i in coupon:
         column = []
         for j in age:
-            column.append(scheduled_balance_percent_for_period(i / 12, 360, j))
+            column.append(schedule_of_ending_balance_percent_for_period(i / 12, 360, j))
         results.append(column)
 
     df = pd.DataFrame(results).T
     df.columns = coupon
     df.index = age
+    return df
+
 
 if __name__ == "__main__":
-    a = create_waterfall()
+    a = example()
     print(a.head())
