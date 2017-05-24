ca863c522c005ef73e3cede3bf166df02246a3eb
Adam Behrman <ab4017@MacBook-Pro-3.local>
2017-05-24 13:39:28 -0400

    added floater_rates to return coupons for floater and inverse floater bonds to to utils.py

--- utils.py
+++ utils.py
@@ -22,3 +22,26 @@ def age_perc(age, e=30):
 
 def burn_perc(factor, f=0.7):
     return 1 - f * (1 - factor)
+
+def floater_rates(floater_size=.75,
+                  inverse_size=.25,
+                  available_coupon=0.09,
+                  margin=0.01,
+                  rate=0.):
+
+    """
+
+    :param floater_size: size of floater bond principal
+    :param inverse_size: size of inverse floater bond principal
+    :param available_coupon: coupon available from underlying collateral
+    :param margin: floater margin to index rate, usually LIBOR or CMT
+    :param rate: rate of LIBOR or CMT to evaluate payment levels
+    :return: coupon payment levels for 'floater', and 'inverse floater'
+    """
+
+    leverage = floater_size / inverse_size
+    floater_rate = min(rate + margin, available_coupon + (1 / leverage * available_coupon))
+
+    inverse_rate = max((available_coupon + leverage * (available_coupon - floater_rate)), 0)
+
+    return floater_rate, inverse_rate
